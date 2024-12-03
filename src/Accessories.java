21ed58fcc6df2f32c1d443bf4dd1ab8692d05c8d
NEo0o0 <nathdanai@gmail.com>
2024-12-03 23:29:38 +0700

    interface

---
+++ src/Accessories.java
@@ -0,0 +1,19 @@
+public interface Accessories {
+    void setLevel(int level);
+}
+
+interface armor extends Accessories {
+    void detail();
+}
+
+interface ring extends Accessories {
+    void detail();
+}
+
+interface boot extends Accessories {
+    void detail();
+}
+
+interface cloak extends Accessories {
+    void detail();
+}
